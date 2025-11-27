6915212a761d3598e22ae759747669af9148418c
DAVE <ro7enkranz@qq.com>
2025-11-28 03:19:18 +0800

    feat: 添加DSS数据可视化工具及相关文档
    添加DSS_DataVisualizer项目文件，包括：
    - 文档图片和GIF展示
    - .vscode配置和.gitignore文件
    - README项目说明文档
    - 针对TMS320F2802x和TMS320F280013x芯片的DSS脚本实现
    
    Signed-off-by: DAVE <ro7enkranz@qq.com>

---
+++ DssScriptFiles/TMS320F280013x/DssScript.java
@@ -0,0 +1,184 @@
+package TMS320F280013x;
+
+import java.util.ArrayList;
+import java.io.IOException;
+import com.ti.ccstudio.scripting.environment.*;
+import com.ti.debug.engine.scripting.*;
+
+// 注意：请勿修改函数的返回值类型、函数名、参数列表，否则上位机无法正常调用被修改的函数，
+// 仅根据需要修改函数体中的代码即可，大部分情况下只需要根据需求改动 execFlashProgram, execDssInit 这两个函数。
+
+public class DssScript {
+    // 获取类文件所处路径
+    private static String sClassFilePath = DssScript.class.getProtectionDomain().getCodeSource().getLocation().getPath()
+            .substring(1) + DssScript.class.getPackage().getName() + "/";
+
+    private ScriptingEnvironment env = null;
+    private DebugServer server = null;
+    private DebugSession session = null;
+
+    // 存储表达式
+    public ArrayList<String> exprArrayList;
+
+    // 存储表达式的计算结果
+    public String[] exprResultArray;
+
+    /**
+     * 执行烧录程序
+     *
+     * @throws ScriptingException
+     * @throws IOException
+     */
+    public void execFlashProgram(String sAppSymbolFile) throws ScriptingException, IOException {
+        env.setScriptTimeout(30000); // 根据实际情况调整超时时间
+        // session.flash.performOperation("Erase");
+
+        env.traceWrite("信息: 开始烧录程序...");
+        session.memory.loadProgram(sAppSymbolFile);
+        session.memory.verifyProgram(sAppSymbolFile);
+        env.traceWrite("信息: 烧录完毕！");
+
+        env.traceWrite("信息: 开始计算校验和...");
+        session.flash.performOperation("CalculateChecksum");
+
+        env.setScriptTimeout(3000);
+
+        // 加载符号文件
+        session.symbol.load(sAppSymbolFile);
+        session.symbol.getSymbolFileName();
+    }
+
+    public void execDssInit(String sConfigurationFile, String sAppSymbolFile, String sLogStylesheetFile) throws ScriptingException {
+        env = ScriptingEnvironment.instance();
+        env.setScriptTimeout(3000); // 不同芯片 DSS 初始化时间不同，需要根据实际情况调整超时时间
+
+        System.out.println("脚本文件所处路径: " + sClassFilePath);
+        // 创建 Log 日志文件
+        env.traceBegin(sClassFilePath + "Logs.xml", sLogStylesheetFile);
+        env.traceSetConsoleLevel(TraceLevel.INFO);
+        env.traceSetFileLevel(TraceLevel.INFO);
+
+        server = (DebugServer) env.getServer("DebugServer.1");
+        server.setConfig(sConfigurationFile);
+        session = server.openSession("*", "*");
+
+        // session.options.printOptions(".*"); // 打印可设置的仿真器选项，不同仿真器选项可能有所不同，需要根据实际情况调整
+        session.options.setBoolean("AutoRunToLabelOnRestart", true);
+        session.options.setBoolean("AutoRunToLabelOnReset", true);
+        session.options.setBoolean("AutoResetOnConnect", true);
+
+        env.traceWrite("信息: 当前工作目录: " + env.getCurrentDirectory());
+        env.traceWrite("信息: 开始连接到目标...");
+        session.target.connect();
+
+        // env.traceWrite("信息: 打印可选的 Flash 设置...");
+        // session.flash.options.printOptionById("FlashProgrammerNode");
+        // env.traceWrite("信息: 列出当前芯片所支持的 Flash 操作...");
+        // session.flash.listSupportedOperations();
+        // NOTE: 不同芯片系列所定义的 Flash 操作字段可能不同，需要根据实际情况调整，可通过上方两个函数打印出所有可选的 Flash 设置字段
+        // 如果不使用上位机的烧录功能，可注释下面的 Flash 操作代码，否则请根据芯片系列调整 Flash 操作字段
+        session.flash.options.setString("FlashEraseSelection", "Entire Flash"); // Entire Flash, Necessary Sectors Only (for Program Load)
+        session.flash.options.setString("FlashDownloadSetting", "Erase and Program"); // Erase and Program, Program Only, Verify Only
+
+        // 如果芯片没有使能 CSM 加密，请注释下面几行代码，如果使能了 CSM 加密，请在下方填写密码，否则无法连接芯片
+        // session.flash.options.setString("Z1CSMPSWD0", "FFFFFFFF");
+        // session.flash.options.setString("Z1CSMPSWD1", "FFFFFFFF");
+        // session.flash.options.setString("Z1CSMPSWD2", "56781234");
+        // session.flash.options.setString("Z1CSMPSWD3", "74471331");
+        // session.flash.performOperation("Z1Unlock");
+
+        // 加载符号文件
+        session.symbol.load(sAppSymbolFile);
+        session.symbol.getSymbolFileName();
+    }
+
+    public void execDssDeinit() throws ScriptingException {
+        env.traceWrite("信息: 断开连接...");
+        session.target.disconnect();
+        session.terminate();
+        server.stop();
+
+        env.traceEnd();
+    }
+
+    /**
+     * 初始化 exprResultArray 数组并设置其元素个数、清空 exprArrayList
+     *
+     * @param num exprResultArray 的元素个数
+     */
+    public void execExpressionInit(int num) {
+        exprResultArray = new String[num];
+        if (exprArrayList != null) {
+            exprArrayList.clear();
+        } else {
+            exprArrayList = new ArrayList<>();
+        }
+
+        System.out.println("信息: num = " + num);
+        System.out.println("信息: exprResultArray.length = " + exprResultArray.length);
+    }
+
+    /**
+     * 执行批量表达式前需要先调用此函数将多个表达式添加到 ArrayList 中
+     */
+    public void execExpressionArrayListAdd(String sExpression) {
+        exprArrayList.add(sExpression);
+    }
+
+    /**
+     * 执行批量表达式（用于通过符号获取变量的值）
+     * @throws ScriptingException
+     */
+    public void execExpressionEvaluate() throws ScriptingException {
+        for (int i = 0; i < exprArrayList.size(); i++) {
+            exprResultArray[i] = session.expression.evaluateToString(exprArrayList.get(i));
+            // System.out.println(i + " - " + exprArrayList.get(i) + " = " + exprResultArray[i]);
+        }
+    }
+
+    /**
+     * 执行单个表达式（例如用于读取寄存器值、通过符号获取变量的值、赋值等操作）
+     * @throws ScriptingException
+     */
+    public String execExpressionEvaluate(String sExpression) throws ScriptingException {
+        return session.expression.evaluateToString(sExpression);
+    }
+
+    public void execDssGo() throws ScriptingException {
+        session.target.runAsynch();
+    }
+
+    public void execDssHalt() throws ScriptingException {
+        session.target.halt();
+    }
+
+    public void execDssReset() throws ScriptingException {
+        session.target.reset();
+        session.target.restart(); // 需要先 session.symbol.load() 加载 .out 符号文件，或者 session.memory.loadProgram() 时使用 .out 符号文件
+    }
+
+    // @formatter:off
+    // 编译
+    // javac -encoding UTF-8 -classpath ".;C:/ti/ccs1281/ccs/ccs_base/DebugServer/packages/ti/dss/java/dss.jar" ./DssScriptFiles/TMS320F280013x/DssScript.java
+    // 运行
+    // java -classpath ".;C:/ti/ccs1281/ccs/ccs_base/DebugServer/packages/ti/dss/java/dss.jar;./DssScriptFiles" TMS320F280013x/DssScript
+    // @formatter:on
+    /**
+     * main 函数仅用于独立测试，不依赖 Qt C++ 上位机，也不会被上位机调用，直接在终端中执行该 .class 即可测试
+     */
+    public static void main(String[] args) {
+        DssScript dss = new DssScript();
+
+        try {
+            dss.execDssInit("D:/CCS12/TestProjF2800132/TMS320F2800132.ccxml", "D:/CCS12/TestProjF2800132/APP.out", "D:/CCS12/TestProjF2800132/LogFileStylesheet.xsl");
+            dss.execFlashProgram("D:/CCS12/TestProjF2800132/APP.out");
+            dss.execDssReset();
+            dss.execDssGo();
+            dss.execDssDeinit();
+        } catch (ScriptingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,89 @@
+<table style = "width: 100%;">
+<tr>
+<td colspan="2">
+
+> [!NOTE]
+> 本仓库用于 DSS_DataVisualizer 的发布，下载请跳转 [发布页面 (当前版本: v0.0.3.3)](https://github.com/DigitalAllianceStudio/DSS_DataVisualizer/releases/latest)
+
+:beers: DSS_DataVisualizer - 适用于 Texas Instruments 德州仪器芯片的非侵入式数据可视化实时示波工具 <a title="Hits" target="_blank" href="https://github.com/DigitalAllianceStudio/DSS_DataVisualizer"><img src="https://hits.b3log.org/DigitalAllianceStudio/DSS_DataVisualizer.svg"></a>
+
+![GIF1](doc/GIF1.gif)
+
+</td>
+</tr>
+<tr>
+<td style>
+
+![ProgramScreenshot7](doc/ProgramScreenshot7.png)
+
+> [!NOTE]
+> Qt 通过 JNI 去跟 DSS 的 Java 接口交互，以访问仿真器的 DebugServer，通过物理接口 JTAG 对芯片进行非侵入式的读写访问，并实时可视化示波显示到用户界面。
+
+## :receipt: 参考资料
+
+- [Java Scripting with DSS](https://software-dl.ti.com/ccs/esd/documents/dss_java_dss_scripting.html)
+- [调试服务器脚本(DSS)入门教程](https://software-dl.ti.com/ccs/esd/training/workshop/ccsv9/dss_fundamentals-workshop-chinese.html)
+- DSS API `C:/ti/ccs1281/ccs/ccs_base/scripting/docs/DS_API/index.html`
+
+## :package: DSS 脚本文件参考示例
+
+[TMS320F2802x/DssScript.java](DssScriptFiles/TMS320F2802x/DssScript.java)
+[TMS320F280013x/DssScript.java](DssScriptFiles/TMS320F280013x/DssScript.java)
+
+</td>
+<td style>
+
+## :receipt: 其它说明
+
+> [!IMPORTANT]
+> 理论上 CCS 支持的芯片和调试器，本软件也都能支持（请自行尝试）
+
+> [!TIP]
+> 功能介绍：
+
+1. 采样项目名称自定义（方便观察）
+2. 支持自定义 GEL 表达式
+3. 支持 GEL 表达式读值，例如 `gVdcbus*gIdcbus`、`-gVar*8.123f+50` 等...
+4. 支持 GEL 表达式写值，例如 `gEnable=1`、`gWriteVar=-5.678f` 等...
+    ![表达式](doc/表达式.png)
+5. 变量增删改查、采样使能、波形使能
+6. 高清示波图（模式有`扫描模式`、`滚动模式`：支持滚动、缩放、平移、游标测量等操作）
+7. 支持`采样数据导出 CSV`
+8. 支持烧录程序
+9. 支持 `运行`、`挂起`、`复位` 操作
+
+</td>
+</tr>
+<tr>
+<td colspan="2">
+
+> [!IMPORTANT]
+> **注意事项：**
+
+- :warning: 若运行报错（如`缺少动态链接库`），请安装 :package: `vcredist_x64.exe` 和 :package: `vc_redist.x64.exe` 运行库。
+
+</td>
+</tr>
+</table>
+
+<div align="center">
+
+### 软件截图
+
+![ProgramScreenshot0](doc/ProgramScreenshot0.png)
+![ProgramScreenshot1](doc/ProgramScreenshot1.png)
+![ProgramScreenshot2](doc/ProgramScreenshot2.png)
+![ProgramScreenshot3](doc/ProgramScreenshot3.png)
+![ProgramScreenshot4](doc/ProgramScreenshot4.png)
+![ProgramScreenshot5](doc/ProgramScreenshot5.png)
+![ProgramScreenshot6](doc/ProgramScreenshot6.png)
+
+### 采样数据导出 CSV
+
+![采样数据导出示例CSV](doc/采样数据导出示例CSV.png)
+
+</div>
+
+----------
+
+:star: Copyright © 2023 - 2025 Digital Alliance Studio. All rights reserved.
